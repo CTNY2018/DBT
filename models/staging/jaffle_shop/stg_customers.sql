11b9027f17a3fb9ee49151cb82e7b240a6057e32
CASEY.TOM@baruchmail.cuny.edu <CASEY.TOM@baruchmail.cuny.edu>
2022-04-21 23:30:41 +0000

    more models added

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,7 +1,5 @@
-with customers as (
 select
-id as customer_id,
-first_name,
+id as customer_id, 
+first_name, 
 last_name
-from `dbt-tutorial.jaffle_shop.customers`
-)
+from dbt-tutorial.jaffle_shop.customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,8 +1,5 @@
-orders as (
 select
-id as order_id,
-user_id as customer_id,
-order_date,
+id as order_id, 
+user_id as customer_id, order_date, 
 status
 from `dbt-tutorial.jaffle_shop.orders`
-)
